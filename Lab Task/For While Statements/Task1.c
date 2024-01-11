5d3524db3a4dd9a7b0a280b25f6ea295d97d4734
smokemh <hasnatraja786@gmail.com>
2024-01-11 19:58:42 +0100

    add: pattern-printing program
    This commit adds a C program that uses a nested for loop to print the following pattern:
    
    **********
    *********
    ********
    *******
    ******
    *****
    ****
    ***
    **
    *
    
    The program includes two nested loops to control the number of '*' characters in each line.

---
+++ Lab Task/For While Statements/Task1.c
@@ -0,0 +1,29 @@
+
+// Write a program to print the following pattern using for loop:
+// **********
+// *********
+// ********
+// *******
+// ******
+// *****
+// ****
+// ***
+// **
+// *
+
+#include <stdio.h>
+
+int main()
+{
+    int i, j;
+    for (i = 0; i <= 10; i++)
+    {
+        for (j = 10; j >= i; j--)
+        {
+            printf("*");
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
