6e680cf9e2d972c26a9b0c4f742925be4437be59
smokemh <hasnatraja786@gmail.com>
2024-01-12 21:50:19 +0100

    add: program to print "Hello World" 10 times using a while loop
    This commit adds a C program that uses a while loop to print the message "Hello World" ten times. The program initializes a counter variable 'x' and iterates through the loop, printing the message during each iteration.

---
+++ Lab Task/For While Statements/Task8.c
@@ -0,0 +1,14 @@
+/* Write a C program to print 10 times “Hello World” using while loop.*/
+
+#include <stdio.h>
+void main()
+{
+    int x;
+    x = 0;
+    while (x < 10)
+    {
+        printf("Hello World\n");
+
+        x++;
+    }
+}
