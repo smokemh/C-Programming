eabd7bb1db6c76c2806c79314412053f757c0055
smokemh <hasnatraja786@gmail.com>
2024-01-12 21:52:26 +0100

    add: program to display n terms of odd natural numbers and their sum
    This commit adds a C program that uses a while loop to display the first n terms of odd natural numbers. The program prompts the user to enter the number of terms (n) and then iterates through the loop to print the numbers. For demonstration purposes, the program currently prints "Hello Worl

---
+++ Lab Task/For While Statements/Task9.c
@@ -0,0 +1,16 @@
+/* Write a program in C to display the n terms of odd natural number and their sum. Go to the editor*/
+
+#include <stdio.h>
+void main()
+{
+    int num, y;
+    printf("Enter The num of times you want to print using loop = ");
+    scanf("%d", &num);
+    y = 0;
+    while (y < num)
+    {
+        printf("Hello World\n");
+
+        y++;
+    }
+}
