69da2c33a75803ea7612214a178b9fb339ff1d76
smokemh <hasnatraja786@gmail.com>
2024-01-13 20:47:02 +0100

    Added // Escape sequence            Description

--- Introduction To C Language/Intro.c
+++ Introduction To C Language/Intro.c
@@ -1,9 +1,17 @@
 // Printing line of text onto console
 
-#include <stdio.h>
+#include <stdio.h> //include Preprocessor Directive
 
-int main(void)
+int main(void) // main function
 {
-    printf("Welcome to C!\n");
+    printf("Welcome to C!\n"); // print line of text
     return 0;
 }
+
+// Escape sequence            Description
+// \n Newline.                  Position the cursor at the beginning of the next line.
+// \t Horizontal tab.                Move the cursor to the next tab stop.
+// \a Alert.                    Produces a sound or visible alert without changing the current
+// cursor position.
+// \\ Backslash.                    Insert a backslash character in a string.
+// \" Double quote.                 Insert a double-quote character in a string.
