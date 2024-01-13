c6b39bedafda9ed65978f18b27de0227ce0b808e
smokemh <hasnatraja786@gmail.com>
2024-01-13 20:48:04 +0100

    add: add C program to print "Welcome to C!" on the console
    This commit adds a simple C program that uses the printf function to display the text "Welcome to C!" on the console. The program is correctly formatted and includes a return statement at the end of the main function for good practice.

--- Introduction To C Language/Intro.c
+++ Introduction To C Language/Intro.c
@@ -8,10 +8,10 @@ int main(void) // main function
     return 0;
 }
 
-// Escape sequence            Description
-// \n Newline.                  Position the cursor at the beginning of the next line.
+// Escape sequence              Description
+// \n Newline.                      Position the cursor at the beginning of the next line.
 // \t Horizontal tab.                Move the cursor to the next tab stop.
-// \a Alert.                    Produces a sound or visible alert without changing the current
+// \a Alert.                        Produces a sound or visible alert without changing the current
 // cursor position.
 // \\ Backslash.                    Insert a backslash character in a string.
 // \" Double quote.                 Insert a double-quote character in a string.
