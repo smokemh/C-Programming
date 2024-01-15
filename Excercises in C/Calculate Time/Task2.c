07d6397a88c8ce70473251de42ff95068a50f417
abdulmoeed786 <abdulmoeed_786@yahoo.com>
2024-01-15 18:03:36 +0100

    Implement user input analysis in C

---
+++ Excercises in C/Calculate Time/Task2.c
@@ -0,0 +1,17 @@
+//Write a Program for  User Input Analyzer
+#include <stdio.h>  
+void main( void )
+{   
+	int num1;          
+printf("Enter Number . = ");   
+scanf("%d", &num1); 
+ if (num1 > 0) {    
+	 if (num1 >= 1000) {    num1 = 1000;  }   
+	 else {       
+		 if (num1 <500) {       num1 =num1*2;       }       
+		 else {          num1 =num1*10;       }        }    }   
+ else {       num1 =num1 + 30;    }         
+ printf("Num1 = %d\n",num1); 
+ }   
+ 
+
