66035133b4d43fe2412e76237fd5700444ce2545
smokemh <hasnatraja786@gmail.com>
2024-01-21 15:59:52 +0100

    add: Implement Sales Commission Calculator program
    This commit adds a C program that calculates the earnings of salespeople based on a commission system. The program takes input for gross sales and computes the earnings, considering a base salary of $200 per week plus a 9% commission on gross sales. The program continues to prompt for sales input until -1 is entered to end the calculation loop.

--- Structured Task Deveolpment in C/Excercises/ex2.c
+++ Structured Task Deveolpment in C/Excercises/ex2.c
@@ -1,4 +1,4 @@
-// (Sales Commission Calculator) One large chemical company pays its salespeople on a commission basis. The salespeople receive $200 per week plus 9% of their gross sales for that week. For example, a salesperson who sells $5000 worth of chemicals in a week receives $200 plus 9% of $5000, or a total of $650. Develop a program that will input each salesperson’s gross sales for last week and will calculate and display that salesperson’s earnings.
+// (Sales Commission Calculator) One large chemical company pays its salespeople on a commission basis. The salespeople receive $200 per week plus 9% of their gross sales for that week. For example, a salesperson who sells $5000 worth of chemicals in a week receives $200 plus 9% of $5000, or a total of $650. Develop a program that will input each salesperson’s gross sales for last week and will calculate and display that salesperson’s earnings.
 
 #include <stdio.h>
 
