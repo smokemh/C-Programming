f051a4678b168c0a47f17eb44c170cbc73d84867
smokemh <hasnatraja786@gmail.com>
2024-02-08 22:32:33 +0100

    infinite loop by incrementing counter in while loop

---
+++ C Program Control/Counter-Controlled Repetition.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+// function main begins program execution
+int main(void)
+{
+    unsigned int counter = 1; // define counter
+    // initialization
+    while (counter <= 10)
+    {
+        // repetition condition
+        printf("%u\n", counter);
+        // display counter
+    }
+    return 0;
+    // indicate that program ended successfully
+} // end function main
